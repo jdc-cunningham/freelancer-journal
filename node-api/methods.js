bf8b76b2758d8f7a89541737d6c4ee89415269b5
Jacob Cunningham <jcun4128@gmail.com>
2024-01-16 17:47:47 -0600

    one endpoint

--- node-api/methods.js
+++ node-api/methods.js
@@ -5,29 +5,71 @@ require('dotenv').config({
   path: './.env'
 });
 
-const addClient = async (req, res) => (
+const _clientExists = async (clientName) => (
   new Promise ((resolve, reject) => {
-    const { name, topics, rate, rate_type, details } = req.body;
-    const now = formatTimeStr(getDateTime());
-
     pool.query(
-      `INSERT INTO clients SET id = ?, name = ?, topics = ?, rate = ?, rate_type = ?, details = ?, created = ?`,
-      [null, name, topics, rate, rate_type, details, now],
+      `SELECT id FROM clients WHERE name = ?`,
+      [clientName],
       (err, qres) => {
         if (err) {
-          console.error('failed to insert client', err);
-          return {
-            err: true,
-          };
+          reject(new Error("failed to search client"));
         } else {
-          return {
+          resolve({
             err: false,
-          };
+            exists: qres.length ? true : false
+          });
         }
       }
     );
   })
 );
+
+const addClient = async (req, res) => {
+  const { name, topics, rate, rate_type, details } = req.body;
+  const now = formatTimeStr(getDateTime());
+
+  let client = null;
+  let clientSearchErr = false;
+
+  try {
+    client = await _clientExists(name);
+  } catch (e) {
+    console.error(e);
+    clientSearchErr = true;
+  }
+
+  if (clientSearchErr) {
+    res.status(400).send({
+      err: true,
+      msg: 'failed to check if client exists'
+    });
+  }
+
+  if (client?.exists) {
+    res.status(400).send({
+      err: true,
+      msg: 'client exists'
+    });
+  }
+
+  pool.query(
+    `INSERT INTO clients SET id = ?, name = ?, topics = ?, rate = ?, rate_type = ?, details = ?, created = ?`,
+    [null, name, topics, rate, rate_type, details, now],
+    (err, qres) => {
+      if (err) {
+        console.error('failed to insert client', err);
+        res.status(400).send({
+          err: true,
+          msg: 'failed to add client'
+        });
+      } else {
+        res.status(201).send({
+          err: false,
+        });
+      }
+    }
+  );
+};
 
 const getClient = async (req, res) => (
   new Promise ((resolve, reject) => {
